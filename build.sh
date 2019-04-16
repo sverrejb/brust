6bb755440447f162f090b9230f5c7656fad74a8c
Sverre Johann Bjørke <Sverre.Bjorke@spk.no>
2019-04-16 15:06:27 +0200

    updated deployscript

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-cweb_version=0.6.16
+cweb_version=0.6.24
 cweb=https://github.com/koute/cargo-web/releases/download/$cweb_version/cargo-web-x86_64-unknown-linux-gnu.gz
 curl -Lo cargo-web.gz $cweb
 gunzip cargo-web.gz
 chmod u+x cargo-web
 
-binaryen_version=version_51
+binaryen_version=version_78
 binaryen=https://github.com/WebAssembly/binaryen/releases/download/$binaryen_version/binaryen-$binaryen_version-x86_64-linux.tar.gz
 curl -Lo binaryen.tar.gz $binaryen
 tar -xzf binaryen.tar.gz
@@ -17,7 +17,7 @@ curl https://sh.rustup.rs -sSf | sh -s - --default-toolchain nightly -y
 source ~/.cargo/env
 rustup target add wasm32-unknown-unknown
 
-./cargo-web deploy --target=wasm32-unknown-unknown --release
+./cargo-web deploy
 
 shopt -s nullglob
 for filename in ./target/deploy/*.wasm
